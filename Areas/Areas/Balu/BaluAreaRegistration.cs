707183bfc99c11af8d36b6e823d565a6222c327e
BALA MURALI KRISHNA PENUMARTHI <Balu5a3@users.noreply.github.com>
2023-12-02 23:25:19 -0500

    Initial commit

---
+++ Areas/Areas/Balu/BaluAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Areas.Areas.Balu
+{
+    public class BaluAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Balu";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Balu_default",
+                "Balu/{controller}/{action}/{id}",
+                new { controller = "Hello", action = "Index", id = UrlParameter.Optional },
+                namespaces : new[] { "Areas.Areas.Balu.Controllers"}
+            );
+        }
+    }
+}
---
+++ Areas/Areas/Balu/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Areas.Areas.Balu.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Balu/Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
